8b910fb4eade3b26cf05e6f66aed645e8ed8bb0b
Bagus Gandhi Pratama <35498095+bagusgandhi@users.noreply.github.com>
2025-04-11 19:41:47 +0700

    feat - Add new components to index.ts

---
+++ src/components/index.ts
@@ -0,0 +1,11 @@
+// Atoms
+
+
+// Molecules
+export { default as StepperItem } from "./moleculs/StepperItem";
+
+// Organisms
+export { default as StepperContent } from "./organisms/StepperContent";
+
+// Templates
+export { default as MainLayout } from "./templates/MainLayout";
