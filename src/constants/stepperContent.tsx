a66c61a5d9219711f7959700fffaaf4bf3785937
Bagus Gandhi Pratama <35498095+bagusgandhi@users.noreply.github.com>
2025-04-11 19:42:25 +0700

    feat - Add stepperContent.tsx to src/constants directory

---
+++ src/constants/stepperContent.tsx
@@ -0,0 +1,21 @@
+import { CheckOutlined, FileDoneOutlined, FileExcelOutlined, FlagOutlined, SignatureOutlined, TeamOutlined, UserOutlined } from "@ant-design/icons";
+
+export const stepperGroup = {
+  Penyusunan: [1, 2, 3],
+  Pembahasan: [4, 5],
+};
+
+export const stepperData = [
+  { title: "Mulai", icon: <FlagOutlined /> },
+  { title: "Proses Legal", icon: <FileDoneOutlined /> },
+  { title: "Data Kurang", icon: <FileExcelOutlined /> },
+  { title: "Review User", icon: <TeamOutlined /> },
+  { title: "Review Mitra", icon: <TeamOutlined /> },
+  { title: "Penandatanganan", icon: <SignatureOutlined /> },
+  { title: "Selesai", icon: <CheckOutlined /> },
+];
+
+export const groupColors: any = {
+  Penyusunan: "bg-blue-100",
+  Pembahasan: "bg-green-100",
+};
